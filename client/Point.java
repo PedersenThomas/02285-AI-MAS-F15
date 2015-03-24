f4f80e04b7463f09e22e7ad3834ffa6cd3646f8f
Thomas Pedersen <s103665@student.dtu.dk>
2015-03-24 13:20:18 +0100

    World now uses Point instead of x and y ints.

--- client/Point.java
+++ client/Point.java
@@ -4,22 +4,16 @@ public class Point {
 	int x;
 	int y;
 	
-	
-	
 	public Point(Point src) {
 		this.x = src.x;
 		this.y = src.y;
 	}
 	
-	
-
 	public Point(int x, int y) {
 		super();
 		this.x = x;
 		this.y = y;
 	}
-
-
 
 	public Point move(Command.dir dir) {
 		Point p = new Point(this);
@@ -40,10 +34,26 @@ public class Point {
 		}
 		return p;
 	}
-	
+
+	@Override
 	public String toString() {
 		return "(" + x + "," + y+ ")";
 	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Point)) {
+			return false;
+		}
+		Point other = (Point)obj;
+		return other.x == this.x && other.y == this.y;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 127;
+		return this.x * prime + 
+			   this.y;
+	}
 	
 }
--- client/World.java
+++ client/World.java
@@ -6,118 +6,122 @@ import java.util.List;
 import client.TestClientValentin.Agent;
 
 public class World {
-	private List< Box > boxes = new ArrayList< Box >();
-	private List< Goal > goals = new ArrayList< Goal >();
-	private List< Agent > agents = new ArrayList< Agent >();	
-	private List< Point > walls = new ArrayList< Point >();	
+	private List<Box> boxes = new ArrayList<Box>();
+	private List<Goal> goals = new ArrayList<Goal>();
+	private List<Agent> agents = new ArrayList<Agent>();
+	private List<Point> walls = new ArrayList<Point>();
 	private int width;
 	private int height;
-	
+
 	public int getNumberOfAgents() {
 		return agents.size();
 	}
-	
+
 	public void setLevelSize(int width, int height) {
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	public Agent getAgent(int i) {
 		return agents.get(i);
 	}
+
 	public void addBox(Box b) {
 		boxes.add(b);
 	}
-	
+
 	public void addWall(int x, int y) {
-		walls.add(new Point(x,y));
+		walls.add(new Point(x, y));
 	}
+
 	public void addGoal(Goal g) {
 		goals.add(g);
 	}
-	
+
 	public void addAgent(Agent a) {
 		agents.add(a);
 	}
-	
-	
+
+	private boolean boxAt(Point point) {
+		for (int i = 0; i < boxes.size(); i++) {
+			if (boxes.get(i).getPosition() == point) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	private boolean boxAt(int x, int y) {
-		for(int i=0;i<boxes.size();i++) {
-			if((boxes.get(i).getPosition().x == x) && (boxes.get(i).getPosition().y == y)) {
+		return boxAt(new Point(x, y));
+	}
+
+	private boolean wallAt(Point point) {
+		for (int i = 0; i < walls.size(); i++) {
+			if (walls.get(i) == point) {
 				return true;
 			}
 		}
+
 		return false;
 	}
-	
-	private boolean wallAt(int x, int y) {
-		for(int i=0;i<walls.size();i++) {
-			if((walls.get(i).x == x) && (walls.get(i).y == y)) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	private boolean isFreeCell(int x, int y) {
-		if(wallAt(x, y))
+
+	private boolean isFreeCell(Point point) {
+		if (wallAt(point))
 			return false;
-		
-		if(boxAt(x, y))
+
+		if (boxAt(point))
 			return false;
-		
+
 		return true;
 	}
-	
-	private Box getBoxAt(int x, int y) {
-		for(int i=0;i<boxes.size();i++) {
-			if((boxes.get(i).getPosition().x == x) && (boxes.get(i).getPosition().y == y)) {
+
+	private Box getBoxAt(Point position) {
+		for (int i = 0; i < boxes.size(); i++) {
+			if (boxes.get(i).getPosition() == position) {
 				return boxes.get(i);
 			}
 		}
 		return null;
 	}
-	
+
 	public int update(Agent a, Command c) {
-		switch(c.actType) {
+		switch (c.actType) {
 		case Move: {
-			Point agentDestPosition= a.position.move(c.dir1);
-			if(isFreeCell(agentDestPosition.x, agentDestPosition.y)) {
+			Point agentDestPosition = a.position.move(c.dir1);
+			if (isFreeCell(agentDestPosition)) {
 				a.position = agentDestPosition;
 			}
 			break;
 		}
 		case Push: {
-			
-			Point boxSrcPosition= a.position.move(c.dir1);
-			Point boxDestPosition= boxSrcPosition.move(c.dir2);
-			
-			if(boxAt(boxSrcPosition.x,boxSrcPosition.y) && 
-			   isFreeCell(boxDestPosition.x, boxDestPosition.y) &&
-			   !Command.isOpposite(c.dir1, c.dir2)) {
+
+			Point boxSrcPosition = a.position.move(c.dir1);
+			Point boxDestPosition = boxSrcPosition.move(c.dir2);
+
+			if (boxAt(boxSrcPosition) && isFreeCell(boxDestPosition)
+					&& !Command.isOpposite(c.dir1, c.dir2)) {
 				a.position = boxSrcPosition;
-				Box b = getBoxAt(boxSrcPosition.x,boxSrcPosition.y);
+				Box b = getBoxAt(boxSrcPosition);
 				b.setPosition(boxDestPosition);
 			}
-			
+
 			break;
 		}
 		case Pull: {
-			Point agentDestPosition= a.position.move(c.dir1);
-			Point boxSrcPosition= a.position.move(c.dir2);
-			
-			if( boxAt(boxSrcPosition.x,boxSrcPosition.y) && 
-				isFreeCell(agentDestPosition.x, agentDestPosition.y) &&
-				!Command.isOpposite(c.dir1, c.dir2)) {
-				Box b = getBoxAt(boxSrcPosition.x,boxSrcPosition.y);
+			Point agentDestPosition = a.position.move(c.dir1);
+			Point boxSrcPosition = a.position.move(c.dir2);
+
+			if (boxAt(boxSrcPosition.x, boxSrcPosition.y)
+					&& isFreeCell(agentDestPosition)
+					&& !Command.isOpposite(c.dir1, c.dir2)) {
+				Box b = getBoxAt(boxSrcPosition);
 				b.setPosition(a.position);
 				a.position = agentDestPosition;
 			}
 			break;
 		}
 		}
-		
+
 		return 0;
 	}
 }
