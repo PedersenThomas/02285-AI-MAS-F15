450cb28b75bc4028bf97f8c9ee11e7528f96bbca
Thomas Pedersen <s103665@student.dtu.dk>
2015-04-11 16:22:39 +0200

    Removed comment code. We don't need Zombie code.

--- client/Intention.java
+++ client/Intention.java
@@ -29,42 +29,24 @@ public class Intention {
 		//Generate the desires, that is, everything the agent might want to achieve.
 		List<Box> boxes = world.getBoxes();	
 		//List<Agent> agents = world.getAgents();  // only one agent
-		List<Goal> goals = new ArrayList<>(world.getGoals());
+		List<Goal> goals = new ArrayList<Goal>(world.getGoals());
 		
 		goals.sort(new GoalComparator(world));
 		
-		/*
-		for(int i=0;i<goals.size();i++) {			
-			Goal goal = goals.get(i);
-			System.err.println(goal.getPosition() + ": " + world.getGoalPriorityScore(goal));
-		}
-		*/
-		
-		List<Intention> intentions = new ArrayList<>();		
+		List<Intention> intentions = new ArrayList<Intention>();		
 		Map<Goal,Map.Entry<Box,Integer>> minGoalDistance = new HashMap<Goal,Map.Entry<Box,Integer>>();
 		
 		// determine for each goal its closest box
 		for(Goal goal:goals) {
 			if(!world.isGoalCompleted(goal)) {
 				for(Box box:boxes) {
-					if(box.getLetter() == goal.getLetter())	{	
+					if(box.getLetter() == goal.getLetter())	{					
 						
 						Boolean boxSuitable = true;
 						Integer boxToGoalDistance = goal.getPosition().distance(box.getPosition());
 						for(Goal otherGoal:goals) {
 							if((otherGoal != goal) &&
 							   (otherGoal.getLetter() == goal.getLetter())) {
-								/*Integer boxToOtherGoalDistance = otherGoal.getPosition().distance(box.getPosition());
-								if(boxToOtherGoalDistance < boxToGoalDistance) {
-									boxSuitable = false;
-									break;																	
-								}
-								
-								if(boxToOtherGoalDistance == boxToGoalDistance) {
-									if(goals.indexOf(goal) > goals.indexOf(otherGoal))
-									boxSuitable = false;
-									break;																	
-								}*/
 								if(otherGoal.getPosition().equals(box.getPosition())) {
 									boxSuitable = false;
 								}
@@ -92,31 +74,6 @@ public class Intention {
 			  return new Intention(intendedGoal, minGoalDistance.get(intendedGoal).getKey());
 		}
 		return null;
-		
-
-		// Create for each unsatisfied goal an intention
-		/*for (Map.Entry<Goal,Map.Entry<Box,Integer>> entry : minGoalDistance.entrySet())
-		{
-			intentions.add(new Intention(entry.getKey(), entry.getValue().getKey()));
-		}
-		
-		// Check if all goals are satisfied
-		if(intentions.isEmpty())
-			return null;
-		
-		int minAgentDistance = intentions.get(0).box.getPosition().distance(agent.getPosition());
-		int resultIdx = 0;
-		
-		// determine which box is closest to the agent
-		for(int i=1;i<intentions.size();i++) {
-			int distance = intentions.get(i).box.getPosition().distance(agent.getPosition());
-			if(distance < minAgentDistance) {
-				resultIdx = i;
-				minAgentDistance = distance;
-			}
-		}
-
-		return intentions.get(resultIdx);*/
 	}
 	
 	
