c658b985ecd63229dfe74d3da4480d1b00e2442e
Thomas Pedersen <s103665@student.dtu.dk>
2015-04-19 08:57:46 +0200

    Removed out-commented code.

--- client/Plan.java
+++ client/Plan.java
@@ -25,8 +25,6 @@ public class Plan {
 		System.err.format( "Search starting with strategy %s\n", strategy );
 		strategy.addToFrontier( new PlannerNode( world, agent.getId() ) );
 
-		//int numUncompletedGoals = world.getNumberOfUncompletedGoals();
-		//int intendedGoalScore = world.getGoalPriorityScore(i.getGoal());
 		int iterations = 0;
 		while ( true ) {
 			iterations++;
@@ -46,25 +44,7 @@ public class Plan {
 			strategy.addToExplored( leafNode );
 			for ( SearchNode n : leafNode.getExpandedNodes() ) {
 				if ( !strategy.isExplored( n ) && !strategy.inFrontier( n ) ) {
-
-					// Check if a completed goal has been destroyed
-//					boolean highPriorityGoalDestroyed = false;
-//					for(Goal g:n.getWorld().getGoals()) {
-//						if((!n.getWorld().isGoalCompleted(g)) &&
-//						    (leafNode.getWorld().isGoalCompleted(g))) {
-//							// Check if the destroyed goal has a higher priority score than the intended goal
-//							//if(n.getWorld().getGoalPriorityScore(g) > intendedGoalScore) {
-//							if(g.getTotalOrder() < i.getGoal().getTotalOrder()) {
-//								//yes -> that's not ok
-//								highPriorityGoalDestroyed = true;
-//							}
-//							break;
-//						}
-//					}
-//					// Everything ok -> add node to frontier
-//					if(!highPriorityGoalDestroyed)
-						strategy.addToFrontier( n );
-
+					strategy.addToFrontier( n );
 				}
 			}
 		}
