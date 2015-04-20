269291652de93c8aaf8e040d673056ccf8f7540e
valentin012 <valentin.kassarnig@gmail.com>
2015-04-20 12:28:01 +0200

    New Environment

--- client/Command.java
+++ client/Command.java
@@ -29,7 +29,6 @@ public class Command {
 
 	public final static Command[] every;
 
-	// changed from private
 	public static boolean isOpposite( dir d1, dir d2 ) {
 		return d1.ordinal() + d2.ordinal() == 3;
 	}
