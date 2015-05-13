f161ec9f2bf264b0804dc2f051d434c244c57078
Thomas Pedersen <s103665@student.dtu.dk>
2015-05-13 11:23:50 +0200

    Only planning for simplified world, will just leave incorrect solutions.
    A lot of the commands found will be invalid.

--- client/Plan.java
+++ client/Plan.java
@@ -46,8 +46,9 @@ public class Plan {
 		strategy = new BestFirstSearch(h);
 		
 		boolean ignoreBoxes = false;
-		World simpleWorld = world.getSimplifiedCopy(agent.getId());
-		strategy.addToFrontier( new PathNode( simpleWorld, world.getAgent(agent.getId()).getPosition(), subIntention.getEndPosition(), ignoreBoxes) );
+//		World simpleWorld = world.getSimplifiedCopy(agent.getId());
+		World correctWorld = new World(world);
+		strategy.addToFrontier( new PathNode( correctWorld, world.getAgent(agent.getId()).getPosition(), subIntention.getEndPosition(), ignoreBoxes) );
 		int iterations = 0;
 		
 		while ( true ) {
@@ -116,8 +117,9 @@ public class Plan {
 		Heuristic h = new AStar(new HeuristicPlannerFunction(subIntention, agent.getId()));
 		strategy = new BestFirstSearch(h);
 
-		World simpleWorld = world.getSimplifiedCopy(agent.getId());
-		strategy.addToFrontier( new PlannerNode( simpleWorld, agent.getId() ) );
+//		World simpleWorld = world.getSimplifiedCopy(agent.getId());
+		World correctWorld = new World(world);
+		strategy.addToFrontier( new PlannerNode( correctWorld, agent.getId() ) );
 
 		int iterations = 0;
 		List<Goal> completedGoals = world.getCompletedGoals();
@@ -167,8 +169,6 @@ public class Plan {
 				}
 			}
 		}
-		//if(commandQueue == null)
-		//	throw new RuntimeException("No plan found for Intention: " + subIntention + " ("+ iterations + " iterations)");
 	}
 
 	public Command execute() {
