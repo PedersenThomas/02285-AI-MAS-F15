f8db0c582018eee5fb0bf5336df51a9d1d6e884b
Thomas Pedersen <s103665@student.dtu.dk>
2015-05-13 10:29:09 +0200

    Updated Agent toString function.

--- client/Client.java
+++ client/Client.java
@@ -204,7 +204,7 @@ public class Client {
 
 		@Override
 		public String toString() {
-			return "AGENT " + id + " Color: " + color + " Position: " + position;
+			return "AGENT " + id + " Color: " + color + " Position: " + position + " Status: " + status;
 		}
 
 		@Override
