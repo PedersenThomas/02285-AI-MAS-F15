1c79b889da773938153227448a30941021d0d3da
valentin012 <valentin.kassarnig@gmail.com>
2015-05-19 09:36:35 +0200

    compile-script updated

--- compileandexecute.bat
+++ compileandexecute.bat
@@ -5,10 +5,38 @@ javac ./*.java ./Heuristic/*.java ./Search/*.java
 
 cd ..
 
-SET LEVEL=SAsokobanLevel96.lvl
-IF NOT "%1"=="" SET LEVEL=%1
+if exist level.save (
+    set /p LASTLEVEL=<level.save
+) else (
+    set LASTLEVEL=MAmultiagentSort.lvl
+)
 
-java -jar server.jar -l levels/%LEVEL% -c "java client.Client" -g
+SET LEVEL=%1
+if "x%LEVEL%"=="x" (
+  set /p LEVEL=Enter level or press enter for %LASTLEVEL%:
+)
+
+if "x%LEVEL%"=="x" (
+  set LEVEL=%LASTLEVEL%
+)
+
+if "%LEVEL:~-4%" neq ".lvl" (
+  set LEVEL=%LEVEL%.lvl
+)
+
+SET DIRECTORY=levels
+if not exist %DIRECTORY%/%LEVEL% (
+    set DIRECTORY=complevels
+)
+
+if not exist %DIRECTORY%/%LEVEL% (
+    @echo Level %LEVEL% not found!
+    exit
+)
+
+@echo %LEVEL%>level.save
+
+java -jar server.jar -l %DIRECTORY%/%LEVEL% -c "java client.Client" -g
 
 taskkill /im java.exe /f
 
